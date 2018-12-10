1a098014d63a18d7ae6d4bed3b1c942258ce033b
Ramalingesha <ramalingesha.lokananda@blackboard.com>
2018-12-10 19:07:59 +0530

    BCLCCU-927: Updated Hijri calendar english converted dates and month names

--- js/foundation-datepicker.js
+++ js/foundation-datepicker.js
@@ -1163,10 +1163,10 @@
             dateModel: Date
         },
         'en-UL':  {
-            days: ["Ahad", "Ithnin", "Thulatha", "Arba'a", "Khams", "Jumu'ah", "Sabt"],
-            daysShort: ["Ahad", "Ithnin", "Thulatha", "Arba'a", "Khams", "Jumu'ah", "Sabt"],
-            daysMin: ["Ahd", "Ith", "Thu", "Arb", "Kha", "Jum", "Sab"],
-            months: ["Muharram", "Safar", "‎Rabi'ul-Awwal", "Rabi'ul-Akhir", "Jumadal-Ula", "Jumadal-Akhir", "‎Rajab", "Sha'ban", "Ramadan", "Syawwal", "‎Dhul-Qa'da", "Dhul-Hijja"],
+            days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+            daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+            daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
+            months: ["Muharram", "Safar", "‎Rabi Al-Awwal", "Rabi Al-Thani", "Jumada Al-Awwal", "Jumada Al-Akhirah", "‎Rajab", "Shaban", "Ramadan", "Shawwal", "‎Dhu Al-Qidah", "Dhu Al-Hijjah"],
             monthsShort: ["Muh", "Saf", "RAw", "RAk", "JAw", "JAk", "Raj", "Sha", "Ram", "Sya", "DhQ", "DhH"],
             today: "Today",
             format: " mm-yyyy"
